b69146b46b70f323f8a4b7e5f8e67d6834841e27
juangjl <juangjl2009@gmail.com>
2022-03-17 14:03:28 +0800

    macOS version VsFlash

---
+++ tools/macos_VsFlash/install.sh
@@ -0,0 +1,10 @@
+export APP=VsFlash
+sudo rm -fR /Users/Shared/$APP
+sudo mkdir  /Users/Shared/$APP
+sudo cp -fR $APP.App/Contents/css		/Users/Shared/$APP
+sudo cp -fR $APP.App/Contents/img		/Users/Shared/$APP
+sudo cp -fR $APP.App/Contents/data	/Users/Shared/$APP
+sudo chmod -R 777 /Users/Shared/$APP/data
+# copy to application
+sudo rm -fR /Applications/$APP.app
+sudo cp -fR $APP.app /Applications
