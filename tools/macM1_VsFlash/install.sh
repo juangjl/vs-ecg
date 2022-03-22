20b02b76e0c5eb71c183ecf5caa618f083e5b30d
juangjl <juangjl2009@gmail.com>
2022-03-22 08:28:11 +0800

    1.02 version  of VsFlash
    Add the serial port detection

--- tools/macM1_VsFlash/install.sh
+++ tools/macM1_VsFlash/install.sh
@@ -5,6 +5,8 @@ sudo cp -fR $APP.App/Contents/css		/Users/Shared/$APP
 sudo cp -fR $APP.App/Contents/img		/Users/Shared/$APP
 sudo cp -fR $APP.App/Contents/data	/Users/Shared/$APP
 sudo chmod -R 777 /Users/Shared/$APP/data
+sudo chmod -R 777 /Users/Shared/$APP/css
+sudo chmod -R 777 /Users/Shared/$APP/img
 # copy to application
 sudo rm -fR /Applications/$APP.app
 sudo cp -fR $APP.app /Applications
